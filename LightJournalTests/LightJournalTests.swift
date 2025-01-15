e03120df699662314ded1c2254b7d796c3a55dbd
Chuci <chuciche@gmail.com>
2025-01-14 19:31:26 -0800

    Initial commit

---
+++ LightJournalTests/LightJournalTests.swift
@@ -0,0 +1,17 @@
+//
+//  LightJournalTests.swift
+//  LightJournalTests
+//
+//  Created by Chuci Chen on 1/14/25.
+//
+
+import Testing
+@testable import LightJournal
+
+struct LightJournalTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
